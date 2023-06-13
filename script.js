54ce56a43410229d72337e040255acaefe609c04
Charles Chavis <chavisca@gmail.com>
2023-06-12 21:30:45 -0400

    function for character length with validation written

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+//declaration for charlength
+let charlength = 8;
 // Array creation
 var specialcharArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "|", "?", "/", "<", ">"];
 var lowercaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -19,3 +21,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Add prompts for password criteria - length, charset (valid if at least one is selected)
+function Prompts() {
+  //ParseInt used to detect for Not a Number
+  charlength = parseInt(prompt("How many characters do you want for your password?  Min: 8, Max: 128")); 
+  
+  //Validation for numerical response
+  if(isNaN(charlength) || charlength < 8 || charlength > 128) {
+    alert("Character length must be a number between 8 - 128.  Please enter a number");
+    return false;}
+}
